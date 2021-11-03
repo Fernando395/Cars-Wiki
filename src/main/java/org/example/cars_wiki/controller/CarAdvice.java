ede2d983f8679a130f67edf58c289467d7483b32
Fernando395 <Fe395rodrigues@gmail.com>
2021-11-03 02:04:08 -0300

    classe CarAdvice criada

---
+++ src/main/java/org/example/cars_wiki/controller/CarAdvice.java
@@ -0,0 +1,18 @@
+package org.example.cars_wiki.controller;
+
+import org.example.cars_wiki.exceptions.CarNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ControllerAdvice
+public class ChampionAdvice {
+    @ResponseBody
+    @ExceptionHandler(CarNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public String handleChampionNotFound(CarNotFoundException e) {
+        return e.getMessage();
+    }
+}
